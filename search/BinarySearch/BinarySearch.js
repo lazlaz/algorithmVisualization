876780c64ab7582108ed71df7a8e760a692c64bf
liuzhai <578091472@qq.com>
2020-08-03 17:42:02 +0800

    增加二分查找算可视化

---
+++ search/BinarySearch/BinarySearch.js
@@ -0,0 +1,35 @@
+function binarySearch(arr, target) {
+  console.log("00");
+  //用来进行可视化跟踪
+  var virtualArr = [];
+
+  if (target == null) {
+    virtualArr.push({ arr: arr });
+    return virtualArr;
+  }
+  var l = 0;
+  var r = arr.length - 1;
+  while (l <= r) {
+    var mid = Math.floor((l + r) / 2);
+
+    var meta = {};
+    meta.arr = arr;
+    meta.range = [l, r];
+    virtualArr.push(meta);
+
+    if (arr[mid] > target) {
+      r = mid - 1;
+    }
+    if (arr[mid] < target) {
+      l = mid + 1;
+    }
+    if (arr[mid] == target) {
+      var meta = {};
+      meta.arr = arr;
+      meta.range = [mid, mid];
+      virtualArr.push(meta);
+      return virtualArr;
+    }
+  }
+  return virtualArr;
+}
